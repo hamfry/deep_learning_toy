63aff56189de371e77d58494846bdf8da8aa6f48
hamidfehri <h.fehri@sheffield.ac.uk>
2019-05-17 14:29:21 +0100

    created network.py and load_data.py upadte the readme file for the dependencies and running

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # deep_learning_toy
-Toy deep learning project for getting some hands-on experience with keras
+Toy deep learning project for getting some hands-on experience with keras and tensorflow - Thanks and credits to https://medium.com/the-andela-way/deep-learning-hello-world-e1fc53ea888
+
+Example to perform a classification on the MNIST dataset.
+
+Download the MNIST image dataset from  http://yann.lecun.com/exdb/mnist/
+
+Install the dependencies:
+python-minst
+tensorflow
+keras
+tqdm
+h5py
+
+Create the following folders in the root you are storing network.py and data_load.py files:
+models: for the trained model to be saved
+data: for the mnist dataset to be extracted into
+
+Once done, run the code using python network.py from the terminal.
+
---
+++ network.py
@@ -0,0 +1,89 @@
+# Necessary imports
+from keras.callbacks import ModelCheckpoint
+
+from keras.layers import (
+    Activation, Conv2D, Dense, Dropout, Flatten, MaxPooling2D)
+
+from keras.models import Sequential, load_model
+
+from load_data import x_test, x_train, y_test, y_train
+
+# Creating the network, using keras sequential model; taking a layer and appending it to the previous layer
+def create_model():
+    """
+    Create the model.
+    """
+    model = Sequential()
+    model.add(Conv2D(
+        filters=10, kernel_size=(4, 4),
+        input_shape=(28, 28, 1), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.4))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(filters=40, kernel_size=(4, 4), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.4))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(10, activation='softmax'))
+    return model
+
+initial_model = create_model()
+
+# Compiling the network
+def compile_model(model):
+    """
+    Compile the model.
+    """
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adadelta',
+        metrics=['categorical_accuracy'])
+    return model
+
+compiled_model = compile_model(initial_model)
+
+
+# Fitting the model (training)
+
+def fit_model(model):
+    """
+    Train the model while storing the best weights.
+    """
+    checkpoint = ModelCheckpoint(
+        filepath='models/mnist.hdf5',
+        monitor='categorical_accuracy',
+        save_best_only=True,
+        mode='max')
+    model.fit(
+        x_train,
+        y_train,
+        epochs=5,
+        batch_size=100,
+        validation_split=0.25,
+        callbacks=[checkpoint])
+    return model
+
+print('\nTraining the model\n')
+trained_model = fit_model(compiled_model)
+
+# Evaluating the model
+def evaluate_model():
+    """
+    Evaluate the model.
+    """
+    best_model = load_model('models/mnist.hdf5')
+    train_score = best_model.evaluate(x_train, y_train)
+    test_score = best_model.evaluate(x_test, y_test)
+    return train_score, test_score
+
+
+print('\nEvaluating the model...\n')
+train_score, test_score = evaluate_model()
+
+print('\nPercentage predicted correctly:\n')
+print('Training:', train_score[1] * 100, '%')
+print('Testing:', test_score[1] * 100, '%')
+
